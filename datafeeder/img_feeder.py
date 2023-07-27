dc6ed308630728f1a404a24c2f9929985cf1060f
yxj <yangxijin5011@163.com>
2023-07-27 15:30:27 +0800

    Initial commit

--- datafeeder/img_feeder.py
+++ datafeeder/img_feeder.py
@@ -1,28 +1,33 @@
 import os
 import numpy as np
-import torchvision
-from skimage.transform import resize
+import torchvision.transforms
 from torch.utils.data import Dataset
-from tools.trans import seq_translation
 from PIL import Image
+from torchvision import transforms
 
 
+# normMean = [0.5049725, 0.46190482, 0.5082291]
+# normStd = [0.014596962, 0.040307738, 0.01857705]
 #               1  2 3 4 5  6 7  8 9  10 11 12 13 14 15 16
 # orbbec pose   0  1 3 4 5  6 7  8 9  10 11 12 13 14 19 20
 # kinectV2 pose 4 21 9 5 10 6 11 7 17 13 18 14 19 15 20 16
 class MyData(Dataset):
-	def __init__(self, root_dir):
-		self.root_dir = root_dir        # dataset文件夹路径
-		self.data_name_list = os.listdir(root_dir)
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir  # dataset文件夹路径
+        self.data_name_list = os.listdir(root_dir)
+        self.transform = transform
 
-	def __getitem__(self, idx):
-		data_file = self.data_name_list[idx]
-		img_path = os.path.join(self.root_dir, data_file)
-		img = Image.open(img_path)
-		img_tensor = torchvision.transforms.ToTensor()(img)
-		label = int(data_file[(data_file.find('A')+1)])
-		return img_tensor, label
+    def __getitem__(self, idx):
+        data_file = self.data_name_list[idx]
+        img_path = os.path.join(self.root_dir, data_file)
+        img = Image.open(img_path).convert('RGB')
+        img_array = np.asarray(img, dtype=np.float32)
 
-	def __len__(self):
-		return len(self.data_name_list)
+        label = int(data_file[(data_file.find('A') + 1)])
+        if self.transform is not None:
+            img_array = self.transform(img_array)
+        # img_array = np.transpose(img_array, [2, 0, 1])
+        return img_array, label
 
+    def __len__(self):
+        return len(self.data_name_list)
--- main.py
+++ main.py
@@ -1,118 +1,61 @@
-import os
+import numpy as np
+from torchvision.transforms import transforms
 
-import torch
-import torchvision
-from torch import nn
-from torch.utils.tensorboard import SummaryWriter
 from model.resnet50 import *
 from datafeeder.img_feeder import MyData
 from torch.utils.data import DataLoader
 import os
-from model.resnet18 import ResNet18
+from tool.train import train_val, plot_loss, plot_acc
 
 os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
 
 if __name__ == '__main__':
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print("using {} device.".format(device))
+
+    if not os.path.exists('./result'):
+        os.makedirs('./result')
+
     # 参数设置
-    num_classes = 60
+    num_classes = 6
     batch_size = 32
     learn_rate = 0.0001
-    epoch = 1000  # 训练轮数
+    epoch = 100  # 训练轮数
     val_accuracy_all = 0
     # 数据集准备
-    train_dir = "D:/HAR/dataset/NTU_dataset_mini/images/train"
-    val_dir = "D:/HAR/dataset/NTU_dataset_mini/images/val"
-    train_data = MyData(train_dir)
-    val_data = MyData(val_dir)
-    # train_data = MyData(train_dir, '0') + MyData(train_dir, '1') + MyData(train_dir, '2') +\
-    # 			 MyData(train_dir, '3') + MyData(train_dir, '4') + MyData(train_dir, '5')
-    # val_data = MyData(val_dir, '0') + MyData(val_dir, '1') + MyData(val_dir, '2') +\
-    # 			 MyData(val_dir, '3') + MyData(val_dir, '4') + MyData(val_dir, '5')
-
+    train_dir = "D:/postgraduate/action-recognition/dataset/NTU_dataset_mini/images/train"
+    val_dir = "D:/postgraduate/action-recognition/dataset/NTU_dataset_mini/images/val"
+    train_data = MyData(train_dir,
+                        transform=transforms.Compose([transforms.ToTensor(),
+                                                      transforms.Normalize(
+                                                          mean=[0.5049725, 0.46190482, 0.5082291],
+                                                          std=[0.014596962, 0.040307738, 0.01857705])]))
+    val_data = MyData(val_dir,
+                      transform=transforms.Compose([transforms.ToTensor(),
+                                                    transforms.Normalize(
+                                                        mean=[0.5049725, 0.46190482, 0.5082291],
+                                                        std=[0.014596962, 0.040307738, 0.01857705])])
+                      )
+    # 训练集大小
     train_data_size = len(train_data)
+    # 测试集大小
     val_data_size = len(val_data)
     print("\ntrain on {}".format(train_data_size))
     print("val on {}".format(val_data_size))
-
     # dataloader加载
     train_loader = DataLoader(dataset=train_data, batch_size=batch_size
-                              , shuffle=True, drop_last=False, num_workers=3)
+                              , shuffle=True, drop_last=False, num_workers=4)
     val_loader = DataLoader(dataset=val_data, batch_size=batch_size
-                            , shuffle=True, drop_last=False, num_workers=3)
-
+                            , shuffle=True, drop_last=False, num_workers=4)
     # 创建网络模型
-    # model = ResNet18()
-    # model = model.cuda()
-    model = ResNet(Bottleneck, [3, 6, 4, 3], 6)
-    model = model.cuda()
-
+    model = ResNet(Bottleneck, [3, 4, 6, 3], 6)
     # 损失函数
-    loss_fn = nn.CrossEntropyLoss().cuda()
+    loss_fn = nn.CrossEntropyLoss()
     # 优化器
     optimizer = torch.optim.Adam(model.parameters(), lr=learn_rate)
+    # train val
+    history = train_val(epoch, model, train_loader, train_data_size, val_loader, val_data_size, loss_fn, optimizer,
+                        device)
 
-    # 设置训练网络的一些参数
-    total_train_step = 0  # 训练次数
-    total_val_step = 0  # 验证次数
-    val_accuracy = 0
-    train_accuracy = 0
-    best_accuracy = 0
-    # 添加tensorboard
-    # writer = SummaryWriter("./logs_train")
-
-    for i in range(epoch):
-        print("-------第{}轮训练开始-------".format(i + 1))
-
-        # 训练
-        model.train()
-        total_train_loss = 0
-        total_accuracy = 0
-        for data in train_loader:
-            body_rbg, targets = data
-            body_rbg = body_rbg.cuda()
-            targets = targets.cuda()
-            outputs = model(body_rbg)
-
-            # out = outputs.cpu()
-            # print(targets)
-            # print(out)
-
-            loss = loss_fn(outputs, targets)
-            # 优化器调优
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            accuracy = (outputs.argmax(1) == targets).sum()
-
-            total_accuracy = total_accuracy + accuracy
-            total_train_loss += loss
-            total_train_step += 1
-            if total_train_step % 30 == 0:
-                print("训练次数：{}, loss：{}".format(total_train_step, loss))
-        # writer.add_scalar("train_loss", loss, total_train_step)
-        print("整体训练集上的loss：{}, 整体训练集上的accu：{}".format(total_train_loss, total_accuracy / train_data_size))
-        if i % 100 == 0 or i == epoch - 1:
-            torch.save(model, "./results/best{}.pt".format(i))
-            print("model saved")
-
-        # 测试步骤
-        model.eval()
-        total_test_loss = 0
-        total_accuracy = 0
-        with torch.no_grad():
-            for data in val_loader:
-                body_rbg, targets = data
-                body_rbg = body_rbg.cuda()
-                targets = targets.cuda()
-                outputs = model(body_rbg)
-                accuracy = (outputs.argmax(1) == targets).sum()
-                total_accuracy = total_accuracy + accuracy
-
-                with torch.no_grad():
-                    loss = loss_fn(outputs, targets)
-                total_test_loss += loss
-        print("整体测试集上的loss：{}, 整体测试集上的accu：{}".format(total_test_loss, total_accuracy / val_data_size))
-
-        total_val_step += 1
-
-# writer.close()
+    plot_loss(np.arange(0, epoch), history)
+    plot_acc(np.arange(0, epoch), history)
